af957c8a0cdb40dc1bd101aed496649d6f5774ec
Joncitone <jarreola7123@gmail.com>
2020-07-29 15:56:49 -0500

    setup Fire.js file and added 'credentials' to git ignore

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 export default class LoginScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+  continue = () => {
+    this.props.navigation.navigate('Chat', { name: this.state.name });
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text> Login Screen </Text>
+        <View style={styles.circle} />
+        <View style={{ marginTop: 64 }}>
+          <Image
+            source={require('../assets/chat.png')}
+            style={{ width: 100, height: 100, alignSelf: 'center' }}
+          />
+        </View>
+        <View style={{ marginHorizontal: 32 }}>
+          <Text style={styles.header}>Username</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Username Here"
+            onChangeText={(name) => {
+              this.setState({ name });
+            }}
+            value={this.state.name}
+          />
+          <View style={{ alignItems: 'flex-end', marginTop: 64 }}>
+            <TouchableOpacity style={styles.continue} onPress={this.continue}>
+              <Ionicons name="md-arrow-round-forward" size={24} color="#FFF" />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -14,7 +52,39 @@ export default class LoginScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#F4F5F7',
+  },
+  circle: {
+    width: 500,
+    height: 500,
+    borderRadius: 500 / 2,
+    backgroundColor: '#FFF',
+    position: 'absolute',
+    left: -120,
+    top: -20,
+  },
+  header: {
+    fontWeight: '800',
+    fontSize: 30,
+    color: '#514E5A',
+    marginTop: 32,
+  },
+  input: {
+    marginTop: 32,
+    height: 50,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#BAB7C3',
+    borderRadius: 30,
+    paddingHorizontal: 16,
+    color: '#514E5A',
+    fontWeight: '600',
+  },
+  continue: {
+    width: 70,
+    height: 70,
+    borderRadius: 70 / 2,
+    backgroundColor: '#9075E3',
     alignItems: 'center',
+    justifyContent: 'center',
   },
 });
